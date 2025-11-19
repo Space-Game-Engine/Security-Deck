d3975c381b5bba35e9109f9a4267dc9419771c06
Krzysztof Wachnicki-Węgrzyn <ptyskju@gmail.com>
2025-11-20 00:06:13 +0100

    Add exception filter

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import {NestFactory} from '@nestjs/core';
 
 import {AppModule} from './app.module';
+import {HttpExceptionFilter} from './exception.filter';
 
 async function bootstrap(): Promise<void> {
 	const app = await NestFactory.create(AppModule);
+	app.useGlobalFilters(new HttpExceptionFilter());
 	await app.listen(process.env.PORT ?? 3010);
 
 	console.log(`App is loaded at ${await app.getUrl()} URL`);
